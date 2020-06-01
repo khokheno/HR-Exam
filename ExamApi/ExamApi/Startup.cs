0c06720826a3b4dd0acf0db4ceb9263a88a42568
abeer282 <abeerala282@gmail.com>
2020-06-01 20:01:06 +0300

    Add service

--- ExamApi/ExamApi/Startup.cs
+++ ExamApi/ExamApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ExamApi;
+using ExamApi.Contexts;
 
 namespace ExamApi
 {
@@ -25,6 +27,7 @@ namespace ExamApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<QuestionContext>();/*ADDED*/
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
