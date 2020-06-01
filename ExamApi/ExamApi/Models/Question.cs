7f354041ff9fa1a13e7bea8fd12221d2aa1f4000
abeer282 <abeerala282@gmail.com>
2020-06-01 19:58:43 +0300

    Add question model

---
+++ ExamApi/ExamApi/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamApi.Models
+{
+    public class Question
+    {
+        public long Id { get; set; }
+        public string question { get; set; }
+        public string answer1 { get; set; }
+        public string answer2 { get; set; }
+        public string answer3 { get; set; }
+        public string answer4 { get; set; }
+        public Int32 true_answer { get; set; }
+        public bool has_time { get; set; }
+        public Int32 time_in_seconds { get; set; }
+        
+    }
+}
